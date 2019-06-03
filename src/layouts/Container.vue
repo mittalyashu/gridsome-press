954b43ec89c175f0ff5b83d99c0a05bdcfa1d1a5
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-03 15:44:52 +0530

    Container layout

---
+++ src/layouts/Container.vue
@@ -0,0 +1,24 @@
+<template>
+	<div :style="{ maxWidth: width }" class="container">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Container",
+	props: {
+		width: {
+			type: String,
+			default: null
+		}
+	}
+};
+</script>
+
+<style lang='sass'>
+.container
+	margin: 0 auto
+	padding-left: 20px
+	padding-right: 20px
+</style>
