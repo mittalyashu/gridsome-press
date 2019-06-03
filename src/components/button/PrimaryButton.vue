91d6e42eb88ed0fd8376bde394c62831b767955d
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-03 15:44:36 +0530

    Primary button component

---
+++ src/components/button/PrimaryButton.vue
@@ -0,0 +1,34 @@
+<template>
+	<g-link :to="link" class="primary-button">
+		{{ text }}
+	</g-link>
+</template>
+
+<script>
+export default {
+	name: "PrimaryButton",
+	props: {
+		text: {
+			type: String,
+			default: ""
+		},
+		link: {
+			type: String,
+			default: ""
+		}
+	}
+}
+</script>
+
+<style lang='sass'>
+.primary-button
+	display: inline-block
+	font-size: 1.2rem
+	color: #fff
+	background-color: #3eaf7c
+	padding: .8rem 1.6rem
+	border-radius: 4px
+	transition: background-color .1s ease
+	box-sizing: border-box
+	border-bottom: 1px solid #389d70
+</style>
