271e14f4507ada94d4b986013da1ac619bef3c75
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-03 15:47:39 +0530

    Add features section to index page

---
+++ src/components/Features.vue
@@ -0,0 +1,53 @@
+<template>
+	<div v-if="features.length > 0" class="features__list">
+		<div v-for="feature in features" class="features__list-item">
+			<h2>{{ feature.title }}</h2>
+			<p>{{ feature.description }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+import featuresData from "../data/features.yml";
+
+export default {
+	name: "Features",
+	computed: {
+		features() {
+			return featuresData;
+		}
+	}
+}
+</script>
+
+<style lang='sass'>
+.features__list
+	display: flex
+	flex-direction: column
+	flex-wrap: wrap
+	align-items: flex-start
+	align-content: stretch
+	justify-content: space-between
+
+	&-item
+		text-align: left
+		flex-grow: 1
+		flex-basis: 30%
+		padding: 0px 1.5rem
+
+		h2
+			font-size: 1.4rem
+			font-weight: 500
+			color: rgb(58, 81, 105)
+
+		p
+			color: rgb(78, 110, 142)
+
+@media (min-width: 719px)
+	.features__list
+		flex-direction: row
+
+		&-item
+			max-width: 30%
+			padding: 0
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -22,6 +22,9 @@
 						class="home__hero-cta"
 					/>
 				</div>
+				<div class="home__features">
+					<features />
+				</div>
 			</div>
 		</container>
 	</Layout>
@@ -30,12 +33,14 @@
 <script>
 import Container from "../layouts/Container";
 import PrimaryButton from "../components/button/PrimaryButton";
+import Features from "../components/Features";
 
 export default {
 	name: "IndexPage",
 	components: {
 		Container,
 		PrimaryButton,
+		Features
 	}
 };
 </script>
@@ -68,4 +73,8 @@ export default {
 		&-cta
 			margin-bottom: 1.8rem
 
+	&__features
+		border-top: 1px solid #eaecef
+		padding: 1.2rem 0
+		margin-top: 2.5rem
 </style>
