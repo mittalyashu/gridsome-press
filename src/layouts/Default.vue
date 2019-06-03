7b75cbfb76dd7197ad62e0324c504502b47d1332
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-03 15:44:18 +0530

    Add new base styles

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -40,5 +40,18 @@ export default {
 };
 </script>
 
+<style lang="sass">
+body
+	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif
+	font-size: 16px
+	-webkit-font-smoothing: antialiased
+	color: rgb(44, 62, 80)
 
+a
+	color: #3eaf7c
+	font-weight: 500
+	text-decoration: none
+
+p, ol, ul
+	line-height: 1.7
 </style>
