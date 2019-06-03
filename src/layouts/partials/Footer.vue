d986db1823d0cad530de94ccd43cd3e2f8c43425
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-03 15:42:00 +0530

    Footer component

---
+++ src/layouts/partials/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+	<footer>
+		MIT Licensed | Copyright © 2018-present {{ siteName }}
+	</footer>
+</template>
+
+<script>
+export default {
+	name: "Footer",
+	props: {
+		siteName: {
+			type: String,
+			default: ""
+		}
+	}
+};
+</script>
+
+<style lang='sass'>
+footer
+	padding: 2.5rem
+	border-top: 1px solid #eaecef
+	text-align: center
+	color: #4e6e8e
+</style>
