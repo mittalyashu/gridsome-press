07c45d1baf481791b3f68dd1ebab52b680c7e70a
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-03 15:43:41 +0530

    Remove old layout styles

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -21,23 +21,10 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
 <script>
 import Header from "./partials/Header";
 import Footer from "./partials/Footer";
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
 export default {
 	name: "DefaultLayout",
 	components: {
@@ -53,15 +40,5 @@ export default {
 };
 </script>
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
 
-.nav__link {
-  margin-left: 20px;
-}
 </style>
