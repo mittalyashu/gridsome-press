50a633843933c6b7fc3060a7f3c3fb68ae7421e7
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-04 11:34:50 +0530

    Remove margin, padding & set box-sizing to all elements

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -37,6 +37,11 @@ export default {
 </script>
 
 <style lang="sass">
+*
+	margin: 0
+	padding: 0
+	box-sizing: border-box
+
 body
 	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif
 	font-size: 16px
